752285339af45be377b59d225ec7efb2800a6df1
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2020-07-04 19:12:27 +0430

    Carousel slider control with navigation and controls

---
+++ Carousel slider control with navigation and controls/js.js
@@ -0,0 +1,30 @@
+const slider = document.querySelector(".slider");
+
+const leftArrow = document.querySelector(".left");
+const rightArrow = document.querySelector(".right");
+const indicatorParents = document.querySelector('.slider-controls ul');
+let sectionIndex = 0;
+
+
+document.querySelectorAll('.slider-controls li').forEach(function (indicator, ind) {
+    indicator.addEventListener('click', function () {
+        sectionIndex = ind;
+        document.querySelector('.slider-controls .selected').classList.remove('selected');
+        indicator.classList.add('selected')
+        slider.style.transform = 'translate(' + (sectionIndex) * -25 +'%)';
+    });
+});
+
+rightArrow.addEventListener('click', function () {
+    sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 3;
+    document.querySelector('.slider-controls .selected').classList.remove('selected');
+    indicatorParents.children[sectionIndex].classList.add('selected');
+    slider.style.transform = 'translate(' + (sectionIndex) * -25 +'%)';
+});
+
+leftArrow.addEventListener('click', function () {
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+    document.querySelector('.slider-controls .selected').classList.remove('selected');
+    indicatorParents.children[sectionIndex].classList.add('selected');
+    slider.style.transform = 'translate(' + (sectionIndex) * -25 +'%)';
+});
