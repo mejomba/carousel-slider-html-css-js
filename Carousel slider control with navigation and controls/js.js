c8fa0b8e04dbf1358c6cc514c80b8e4b1fcbddbf
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2020-07-04 20:10:50 +0430

    now config for any number of slide's

--- Carousel slider control with navigation and controls/js.js
+++ Carousel slider control with navigation and controls/js.js
@@ -4,27 +4,28 @@ const leftArrow = document.querySelector(".left");
 const rightArrow = document.querySelector(".right");
 const indicatorParents = document.querySelector('.slider-controls ul');
 let sectionIndex = 0;
+let numberOfSlide = 10;
 
 
 document.querySelectorAll('.slider-controls li').forEach(function (indicator, ind) {
     indicator.addEventListener('click', function () {
         sectionIndex = ind;
         document.querySelector('.slider-controls .selected').classList.remove('selected');
-        indicator.classList.add('selected')
-        slider.style.transform = 'translate(' + (sectionIndex) * -25 +'%)';
+        indicator.classList.add('selected');
+        slider.style.transform = 'translate(' + (sectionIndex) * -100/numberOfSlide +'%)';
     });
 });
 
 rightArrow.addEventListener('click', function () {
-    sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 3;
+    sectionIndex = (sectionIndex < numberOfSlide-1) ? sectionIndex + 1 : numberOfSlide-1;
     document.querySelector('.slider-controls .selected').classList.remove('selected');
     indicatorParents.children[sectionIndex].classList.add('selected');
-    slider.style.transform = 'translate(' + (sectionIndex) * -25 +'%)';
+    slider.style.transform = 'translate(' + (sectionIndex) * -100/numberOfSlide +'%)';
 });
 
 leftArrow.addEventListener('click', function () {
     sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
     document.querySelector('.slider-controls .selected').classList.remove('selected');
     indicatorParents.children[sectionIndex].classList.add('selected');
-    slider.style.transform = 'translate(' + (sectionIndex) * -25 +'%)';
+    slider.style.transform = 'translate(' + (sectionIndex) * -100/numberOfSlide +'%)';
 });
