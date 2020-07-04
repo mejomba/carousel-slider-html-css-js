2e353d3d6a7fc2ab5c8dff18acbc08868c28cd62
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2020-07-04 23:39:11 +0430

    infinite loop carousel slider

---
+++ infinite loop carousel slider/js.js
@@ -0,0 +1,36 @@
+const slider = document.querySelector('.slider');
+const carousel = document.querySelector('.carousel');
+const right = document.querySelector('.right');
+const left = document.querySelector('.left');
+var jahat;
+let slideNumber = 8;
+
+left.addEventListener('click', function () {
+    if (jahat === -1) {
+        slider.appendChild(slider.firstElementChild);
+        jahat = 1;
+    }
+    jahat = 1;
+    carousel.style.justifyContent = 'flex-end';
+    slider.style.transform = 'translate('+ (100/slideNumber)+'%)';
+});
+
+right.addEventListener('click', function () {
+    jahat = -1;
+    carousel.style.justifyContent = 'flex-start';
+    slider.style.transform = 'translate('+ (-100/slideNumber)+'%)';
+});
+
+slider.addEventListener('transitionend', function () {
+    if (jahat === -1) {
+        slider.appendChild(slider.firstElementChild);
+    } else if (jahat === 1) {
+        slider.prepend(slider.lastElementChild);
+    }
+
+    slider.style.transition = 'none';
+    slider.style.transform = 'translate(0)';
+    setTimeout(function () {
+        slider.style.transition = 'all 0.5s';
+    });
+});
